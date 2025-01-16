e9498fd3aff5fcbd51eb897550193dfc55a3b53d
kaushik0010 <paykolikaushikb@gmail.com>
2025-01-16 17:20:33 +0530

    index update

--- Backend/db/connectDB.js
+++ Backend/db/connectDB.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
     try {
         // console.log("mongo_uri: ", process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected successfully`)
     } catch (error) {
         console.log("Error connecting to MongoDB: ", error.message)
         process.exit(1);
--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import path from 'path';
 
 import { connectDB } from './db/connectDB.js';
 
@@ -12,7 +11,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -21,13 +19,6 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/Frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"))
-    })
-}
 
 app.listen(PORT, () => {
     connectDB();
